8802d9aba2664472c81697b6ea4deaa5e3093b86
徐琬璇 <xuwanxuan@magicgirl.local>
2017-04-27 14:09:46 +0800

    Initial Commit

---
+++ Notes/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Notes
+//
+//  Created by 徐琬璇 on 2017/4/27.
+//  Copyright © 2017年 noneTobacco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
