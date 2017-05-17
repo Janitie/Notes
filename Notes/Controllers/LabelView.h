64671a406e9afd070822f9ddfde833424abdaa69
徐琬璇 <xuwanxuan@magicGirl.local>
2017-05-17 14:27:45 +0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit ae4737addf3eb6b68c4e779ab31fd1f12f9e9996, reversing
    changes made to 666e5f6862cdae8f90d10fc20ca2705be215dd6b.

---
+++ Notes/Controllers/LabelView.h
@@ -0,0 +1,13 @@
+//
+//  LabelView.h
+//  Notes
+//
+//  Created by 徐琬璇 on 2017/5/15.
+//  Copyright © 2017年 noneTobacco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LabelView : UIView
+
+@end
